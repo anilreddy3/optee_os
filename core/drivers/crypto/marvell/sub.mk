c1f18a71d5d87e294918bd6663638a11951d1d3d
Anil Kumar Reddy <areddy3@marvell.com>
2025-05-27 11:17:03 +0530

    crypto: marvell: Add crypto engine support for cn20k
    Add eHSM AES crypto engine support for cn20k,
    MAILBOX-1 is used for authenc and cipher crypto
    offload operations.
    
    Signed-off-by: Anil Kumar Reddy <areddy3@marvell.com>

--- core/drivers/crypto/marvell/sub.mk
+++ core/drivers/crypto/marvell/sub.mk
@@ -8,5 +8,13 @@ ifneq (,$(filter $(PLATFORM_FLAVOR),cn10ka cn10kb cnf10ka cnf10kb))
 srcs-y += cn10k/ehsm.c cn10k/ehsm-aes.c
 incdirs-y += cn10k/include
 endif
+ifneq (,$(filter $(PLATFORM_FLAVOR), cn20ka cnf20ka))
+
+# eHSM crypto engine Contest store/load support
+$(call force,CFG_EHSM_CONTEXT_STORE_SUPPORT,y)
+
+srcs-y += cn20k/ehsm.c cn20k/ehsm-aes.c
+incdirs-y += cn20k/include
+endif
 endif
 
